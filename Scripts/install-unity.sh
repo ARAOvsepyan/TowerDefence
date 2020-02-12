ef2e97dd85307986e2fd19cf18f23893d7f2205b
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2020-02-12 14:17:45 +0300

    Update install-unity.sh

--- Scripts/install-unity.sh
+++ Scripts/install-unity.sh
@@ -1,42 +1,29 @@
-#! /bin/sh
-
-# See https://unity3d.com/get-unity/download/archive
-# to get download URLs
-UNITY_DOWNLOAD_CACHE="$(pwd)/unity_download_cache"
-UNITY_OSX_PACKAGE_URL="https://unity3d.com/get-unity/download?thank-you=update&download_nid=63355&os=Mac"
-UNITY_WINDOWS_TARGET_PACKAGE_URL="https://beta.unity3d.com/download/46dda1414e51/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-2017.2.0f3.pkg"
-
-
-# Downloads a file if it does not exist
-download() {
-
-	URL=$1
-	FILE=`basename "$URL"`
-	
-	# Downloads a package if it does not already exist in cache
-	if [ ! -e $UNITY_DOWNLOAD_CACHE/`basename "$URL"` ] ; then
-		echo "$FILE does not exist. Downloading from $URL: "
-		mkdir -p "$UNITY_DOWNLOAD_CACHE"
-		curl -o $UNITY_DOWNLOAD_CACHE/`basename "$URL"` "$URL"
-	else
-		echo "$FILE Exists. Skipping download."
-	fi
-}
-
-# Downloads and installs a package from an internet URL
-install() {
-	PACKAGE_URL=$1
-	download $1
-
-	echo "Installing `basename "$PACKAGE_URL"`"
-	sudo installer -dumplog -package $UNITY_DOWNLOAD_CACHE/`basename "$PACKAGE_URL"` -target /
-}
-
-
-
-echo "Contents of Unity Download Cache:"
-ls $UNITY_DOWNLOAD_CACHE
-
-echo "Installing Unity..."
-install $UNITY_OSX_PACKAGE_URL
-install $UNITY_WINDOWS_TARGET_PACKAGE_URL
+#!/bin/shproject="GodGame"echo "Attempting to build $project for Windows"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity 
+  -batchmode 
+  -nographics 
+  -silent-crashes 
+  -logFile $(pwd)/unity.log 
+  -projectPath $(pwd) 
+  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" 
+  -quitecho "Attempting to build $project for OS X"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity 
+  -batchmode 
+  -nographics 
+  -silent-crashes 
+  -logFile $(pwd)/unity.log 
+  -projectPath $(pwd) 
+  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" 
+  -quitecho "Attempting to build $project for Linux"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity 
+  -batchmode 
+  -nographics 
+  -silent-crashes 
+  -logFile $(pwd)/unity.log 
+  -projectPath $(pwd) 
+  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" 
+  -quitecho 'Logs from build'
+cat $(pwd)/unity.logecho 'Attempting to zip builds'
+zip -r $(pwd)/Build/linux.zip $(pwd)/Build/linux/
+zip -r $(pwd)/Build/mac.zip $(pwd)/Build/osx/
+zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
