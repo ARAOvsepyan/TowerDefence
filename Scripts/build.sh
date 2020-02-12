b7749964859150fff4b3803697a46645fdd385d4
Ara Ovsepyan <32188396+ARAOvsepyan@users.noreply.github.com>
2020-02-12 14:17:24 +0300

    Update build.sh

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,39 +1,15 @@
-#! /bin/sh
-
-# Example build script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
-
-# Change this the name of your project. This will be the name of the final executables as well.
-project="ci-build"
-
-echo "Attempting to build $project for Windows"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" \
-  -quit
-
-echo "Attempting to build $project for OS X"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildOSXUniversalPlayer "$(pwd)/Build/osx/$project.app" \
-  -quit
-
-echo "Attempting to build $project for Linux"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile $(pwd)/unity.log \
-  -projectPath $(pwd) \
-  -buildLinuxUniversalPlayer "$(pwd)/Build/linux/$project.exe" \
-  -quit
-
-echo 'Logs from build'
-cat $(pwd)/unity.log
+#!/bin/shBASE_URL=http://netstorage.unity3d.com/unity
+HASH=46dda1414e51
+VERSION=2017.2.0f3download() {
+  file=$1
+  url="$BASE_URL/$HASH/$package"  echo "Downloading from $url: "
+  curl -o `basename "$package"` "$url"
+}install() {
+  package=$1
+  download "$package"  echo "Installing "`basename "$package"`
+  sudo installer -dumplog -package `basename "$package"` -target /
+}# See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
+# of available packages, where PLATFORM is `osx` or `win`install "MacEditorInstaller/Unity-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Mac-Support-for-Editor-$VERSION.pkg"
+install "MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-$VERSION.pkg"
